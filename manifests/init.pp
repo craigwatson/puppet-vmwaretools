ff7c3e76c32875ec74a7c36619effc38325a8fe1
Craig Watson <craig@cwatson.org>
2012-11-29 18:30:04 +0000

    Syntax

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 class vmwaretools (
-  $version              = '8.6.5-621624'
-  $working_dir          = '/opt/vmware'
-  $redhat_install_devel = false
+  $version              = '8.6.5-621624',
+  $working_dir          = '/opt/vmware',
+  $redhat_install_devel = false,
 ) {
 
   include vmwaretools::install
