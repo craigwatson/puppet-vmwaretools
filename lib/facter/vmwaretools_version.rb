4731333bc78b743d557aecc07c06d49139e705c3
Craig Watson <craig@cwatson.org>
2013-05-09 13:26:06 +0100

    Adding "not installed" result to vmwaretools_version fact

--- lib/facter/vmwaretools_version.rb
+++ lib/facter/vmwaretools_version.rb
@@ -8,6 +8,8 @@ Facter.add(:vmwaretools_version) do
   setcode do
     if File::executable?("/usr/bin/vmware-toolbox-cmd")
       Facter::Util::Resolution.exec("/usr/bin/vmware-toolbox-cmd -v").sub(%r{(\d*\.\d*\.\d*)\.\d*\s+\(build(-\d*)\)},'\1\2')
+    else
+      "not installed"
     end
   end
 end
