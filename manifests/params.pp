c78df95f64e93c91ad9d69cbf135fa601514ac5a
Craig Watson <craig@cwatson.org>
2013-05-09 15:46:51 +0100

    Prevent_upgrade logic

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,7 @@
 # == Class: vmwaretools::params
 #
-# This class handles parameters for the vmwaretools module
+# This class handles parameters for the vmwaretools module, including the logic
+# that decided if we should install a new version of VMware Tools.
 #
 # == Actions:
 #
@@ -18,11 +19,15 @@
 class vmwaretools::params {
 
   if $::vmwaretools_version == 'not installed' {
+    # If nothing is installed, deploy.
     $deploy_files = true
   } else {
+
+    # If tools are installed, are we handling downgrades?
     if $vmwaretools::prevent_downgrade {
 
-      if versioncmp($::vmwaretools_versionvmwaretools::version,$::vmwaretools_version) < 0 {
+      if versioncmp($::vmwaretools_version,$vmwaretools::version) < 0 {
+        # Only deploy if the installed version is **lower than** the Puppet version
         $deploy_files = true
       } else {
         $deploy_files = false
@@ -30,6 +35,7 @@ class vmwaretools::params {
 
     } else {
 
+      # If we're not handling downgrades, 
       $deploy_files = $::vmwaretools_version ? {
         $vmwaretools::version => false,
         default               => true,
