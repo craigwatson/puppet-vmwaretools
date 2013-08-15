0ad90feb3ad047f4f6f802702bf140ba44d26610
Francois Deppierraz <francois.deppierraz@nimag.net>
2013-08-15 11:55:37 +0200

    Handle case where facter is run as a non-root user

--- lib/facter/esx_version.rb
+++ lib/facter/esx_version.rb
@@ -10,27 +10,31 @@ Facter.add(:esx_version) do
     if File::executable?("/usr/sbin/dmidecode")
       result = Facter::Util::Resolution.exec("/usr/sbin/dmidecode 2>&1")
       if result
-        bios_address = /^BIOS Information$.+?Address: (0x[0-9A-F]{5})$/m.match(result)[1]
+        begin
+          bios_address = /^BIOS Information$.+?Address: (0x[0-9A-F]{5})$/m.match(result)[1]
 
-        case bios_address
-          when '0xE8480'
-            '2.5'
-          when '0xE7C70'
-            '3.0'
-          when '0xE7910'
-            '3.5'
-          when '0xEA6C0'
-            '4'
-          when '0xEA550'
-            '4 update 1'
-          when '0xEA2E0'
-            '4.1'
-          when '0xE72C0'
-            '5'
-          when '0xEA0C0'
-            '5.1'
-          else
-            "unknown, please report #{bios_address}"
+          case bios_address
+            when '0xE8480'
+              '2.5'
+            when '0xE7C70'
+              '3.0'
+            when '0xE7910'
+              '3.5'
+            when '0xEA6C0'
+              '4'
+            when '0xEA550'
+              '4 update 1'
+            when '0xEA2E0'
+              '4.1'
+            when '0xE72C0'
+              '5'
+            when '0xEA0C0'
+              '5.1'
+            else
+              "unknown, please report #{bios_address}"
+          end
+        rescue
+          'n/a'
         end
       end
     end
