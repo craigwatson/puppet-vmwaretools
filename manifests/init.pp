e9cf131e93ce95ddde20580aab95beb0ee30433c
Craig Watson <craig.watson@secretsales.com>
2014-08-26 13:40:47 +0100

    Only parsing the module on Linux kernels (thanks @thesysadm) - fixes #47 and supercedes #48.

--- manifests/init.pp
+++ manifests/init.pp
@@ -105,7 +105,7 @@ class vmwaretools (
   # https://tickets.puppetlabs.com/browse/FACT-151
   # https://projects.puppetlabs.com/issues/3704
 
-  if $::is_virtual == 'true' and $::virtual == 'vmware' {
+  if $::is_virtual == 'true' and $::virtual == 'vmware' and $::kernel == 'Linux' {
 
     if $::vmwaretools_version == undef {
       fail 'vmwaretools_version fact not present, please check your pluginsync configuraton.'
@@ -133,7 +133,7 @@ class vmwaretools (
     if $timesync != undef {
       include vmwaretools::timesync
     }
-  } elsif $fail_on_non_vmware == true {
+  } elsif $fail_on_non_vmware == true and and $::is_virtual == 'false' and $::virtual != 'vmware' {
     fail 'Not a VMware platform.'
   }
 }
